42c8252784251b7f8e3de9809b8e20eaf9b26831
Ragin <ragin666@gmail.com>
2020-06-08 11:49:18 +0200

    version check

---
+++ app/utils/version/version_checker.go
@@ -0,0 +1,62 @@
+package version
+
+import (
+	"bytes"
+	"github.com/hashicorp/go-version"
+	"io/ioutil"
+	"k8s-management-go/app/utils/logger"
+	"net/http"
+)
+
+func CheckVersion() bool {
+	log := logger.Log()
+
+	remoteVersion, err := receiveVersionFromGit()
+	if err != nil {
+		log.Error(err)
+		return false
+	}
+	localVersion, err := readLocalVersion()
+	if err != nil {
+		log.Error(err)
+		return false
+	}
+
+	if remoteVersion == "" || localVersion == "" {
+		return false
+	}
+
+	semVerRemote, err := version.NewSemver(remoteVersion)
+	semVerLocal, err := version.NewSemver(localVersion)
+
+	if semVerLocal.LessThan(semVerRemote) {
+		return true
+	}
+
+	return false
+}
+
+func readLocalVersion() (version string, err error) {
+	read, err := ioutil.ReadFile("VERSION")
+	if err != nil {
+		return "", err
+	}
+	return string(read), nil
+}
+
+func receiveVersionFromGit() (version string, err error) {
+	resp, err := http.Get("https://raw.githubusercontent.com/Ragin-LundF/k8s-jcasc-management-go/master/VERSION")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	buffer := new(bytes.Buffer)
+	_, err = buffer.ReadFrom(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	version = string(buffer.Bytes())
+
+	return version, nil
+}
--- k8s-jcasc-mgmt.go
+++ k8s-jcasc-mgmt.go
@@ -3,11 +3,18 @@ package main
 import (
 	"k8s-management-go/app/cli"
 	"k8s-management-go/app/utils/setup"
+	"k8s-management-go/app/utils/version"
 )
 
 func main() {
 	// setup the system and read config
 	setup.Setup()
+	// check version
+	newVersionAvailable := version.CheckVersion()
+	info := ""
+	if newVersionAvailable {
+		info = "A new version is available!"
+	}
 	// start UI workflow
-	cli.Workflow("", nil)
+	cli.Workflow(info, nil)
 }
