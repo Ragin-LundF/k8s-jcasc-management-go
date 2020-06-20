46d7f5450314e2eae79fb6bbd7cd410184c3fc13
Ragin666 <ragin666@gmail.com>
2020-06-20 20:50:14 +0200

    * gui test with fyne.io

--- app/gui/install/install_screen.go
+++ app/gui/install/install_screen.go
@@ -1,6 +1,7 @@
 package install
 
 import (
+	"fmt"
 	"fyne.io/fyne"
 	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
@@ -10,6 +11,7 @@ import (
 )
 
 func ScreenInstall() fyne.CanvasObject {
+	// Namespace
 	selectNamespaceEntry := widget.NewSelectEntry(findNamespacesForSelect(nil))
 	selectNamespaceEntry.PlaceHolder = "Type or select namespace"
 	selectNamespaceEntry.OnChanged = func(input string) {
@@ -17,13 +19,35 @@ func ScreenInstall() fyne.CanvasObject {
 		selectNamespaceEntry.SetOptions(namespaces)
 	}
 
+	// Deployment name
 	deploymentNameEntry := widget.NewEntry()
 	deploymentNameEntry.SetPlaceHolder("Deployment name")
+	if models.GetConfiguration().Jenkins.Helm.Master.DeploymentName != "" {
+		deploymentNameEntry.Text = models.GetConfiguration().Jenkins.Helm.Master.DeploymentName
+		deploymentNameEntry.Disable()
+	}
+
+	// Install or update
+	radioInstallType := widget.NewRadio([]string{"install", "upgrade"}, func(s string) { fmt.Println("selected", s) })
+	radioInstallType.SetSelected("install")
+
+	form := &widget.Form{
+		Items: []*widget.FormItem{
+			{Text: "Namespace", Widget: selectNamespaceEntry},
+			{Text: "Deployment Name", Widget: deploymentNameEntry},
+			{Text: "Installation type", Widget: radioInstallType},
+		},
+		OnSubmit: func() {
+			fyne.CurrentApp().SendNotification(&fyne.Notification{
+				Title:   fmt.Sprintf("%s of %s started", radioInstallType.Selected, deploymentNameEntry.Text),
+				Content: fmt.Sprintf("Starting on namespace [%s]...", selectNamespaceEntry.Text),
+			})
+		},
+	}
 
 	box := widget.NewVBox(
-		&*selectNamespaceEntry,
-		deploymentNameEntry,
 		widget.NewHBox(layout.NewSpacer()),
+		form,
 	)
 
 	return box
