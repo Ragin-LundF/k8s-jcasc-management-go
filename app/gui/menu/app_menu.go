787c3662fe1b032995d0e308a883a3d4ce11a7b8
Ragin666 <ragin666@gmail.com>
2020-06-28 18:07:05 +0200

    Removing unneeded menu items
    Signed-off-by: Ragin666 <ragin666@gmail.com>

--- app/gui/menu/app_menu.go
+++ app/gui/menu/app_menu.go
@@ -7,29 +7,13 @@ import (
 
 func CreateMainMenu(app fyne.App) *fyne.MainMenu {
 	// K8S Management Menu
-	newItem := createNewSegment()
 	settingsItem := fyne.NewMenuItem("Configuration", func() { fmt.Println("Menu Settings") })
 	quitItem := fyne.NewMenuItem("Quit", func() { app.Quit() })
 
-	findItem := fyne.NewMenuItem("Find", func() { fmt.Println("Menu Find") })
-	helpMenu := fyne.NewMenu("Help", fyne.NewMenuItem("Help", func() { fmt.Println("Help Menu") }))
-
 	mainMenu := fyne.NewMainMenu(
 		// a quit item will be appended to our first menu
-		fyne.NewMenu("K8S Management", newItem, fyne.NewMenuItemSeparator(), settingsItem, fyne.NewMenuItemSeparator(), quitItem),
-		fyne.NewMenu("Edit", fyne.NewMenuItemSeparator(), findItem),
-		helpMenu,
+		fyne.NewMenu("K8S Management", fyne.NewMenuItemSeparator(), settingsItem, fyne.NewMenuItemSeparator(), quitItem),
 	)
 
 	return mainMenu
-}
-
-func createNewSegment() *fyne.MenuItem {
-	newItem := fyne.NewMenuItem("New Project", nil)
-	newItem.ChildMenu = fyne.NewMenu("",
-		fyne.NewMenuItem("Project with Jenkins", func() { fmt.Println("Menu New->Other->Project") }),
-		fyne.NewMenuItem("Project for deployment", func() { fmt.Println("Menu New->Other->Mail") }),
-	)
-
-	return newItem
 }
