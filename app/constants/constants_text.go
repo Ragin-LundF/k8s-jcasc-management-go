1f7c3bada12e729f32448da42eb040a89dc499ed
Ragin666 <ragin666@gmail.com>
2020-10-18 13:16:40 +0200

    NO SONAR
    Signed-off-by: Ragin666 <ragin666@gmail.com>

--- app/constants/constants_text.go
+++ app/constants/constants_text.go
@@ -1,16 +1,21 @@
 package constants
 
-// NOSONAR
-const TextPasswordForSecretsFile = "Password for secrets file"
-const TextPasswordForSecretsFileConfirmation = "Confirmation password for secrets file"
-const TextPasswordDidNotMatch = "Passwords did not match! "
+// Password related texts
+const TextPasswordForSecretsFile = "Password for secrets file"                          // NOSONAR
+const TextPasswordForSecretsFileConfirmation = "Confirmation password for secrets file" // NOSONAR
+const TextPasswordDidNotMatch = "Passwords did not match! "                             // NOSONAR
 
+// Load balancer related texts
 const TextEnterLoadBalancerIPAddress = "Enter the load balancer IP address"
 
+// Jenkins system message related texts
 const TextEnterJenkinsSystemMessageOrLeaveEmptyForDefault = "Enter the Jenkins system message or leave empty for default"
 
+// Jobs configuration related texts
 const TextEnterJobsConfigurationRepository = "Enter jobs configuration repository"
 
+// Namespace related texts
 const TextEnterNamespaceName = "Enter namespace name"
 
+// PVC related texts
 const TextEnterExistingPvcOrLeaveEmpty = "Enter existing Persistent Volume Claim (PVC) or leave empty for emptyDir"
